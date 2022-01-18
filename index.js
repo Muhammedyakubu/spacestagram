4f8ef3808e4e966b691634eeb75d0537fcb0ce46
Muhammed Yakubu <72368510+Muhammedyakubu@users.noreply.github.com>
2022-01-18 16:37:35 -0500

    added like retention feature and improve styling

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 const api_endpoint = "https://api.nasa.gov/planetary/apod?api_key=6uLxa7baK4KCnAAImmcuqiobiJsyPl3rnMbIdcMS";
 
-let images = localStorage.getItem("NASA_images")
-if(images) {
-    images = JSON.parse(images);
-} else {
-    images = []
-}
-
-function appendImage(image) {
+function renderImage(image) {
     const el = document.createElement('div')
     el.classList.toggle("post");
     el.innerHTML = `
     <img src=${image.url} alt="">
     <div class="details">
 
-    <div class="post-header">
-        <h4 class="title">${image.title}</h4>
-        <h4 class="date">${image.date}</h4>
-    </div>
+        <div class="post-header">
+            <h3 class="title">${image.title}</h3>
+            <h3 class="date">${image.date}</h3>
+        </div>
 
-    <p class="description">${image.explanation}</p>
-
-    <button class="like">Like</button>
+        <p class="description">${image.explanation}</p>
         
-    </div>`
+    </div>
+    <button class=${image.likeState} data-postid=${image.postid}>LIKE</button>`
 
-    el.firstElementChild.onclick = function () {window.open(image.hdurl, "_blank")}
-
+    const postImage = el.firstElementChild;
+    const likeButton = el.lastElementChild;
+    postImage.onclick = () => {window.open(image.hdurl, "_blank")}
+    likeButton.onclick = toggleLikeState;
+    
     document.querySelector("#main").appendChild(el);
+}
+
+function toggleLikeState(e) {
+    const likeBtn = e.target;
+    if (likeBtn.classList == "not-liked"){
+        likeBtn.classList = "liked" 
+        images[likeBtn.dataset.postid].likeState = "liked"
+    } else {
+        likeBtn.classList = "not-liked"
+        images[likeBtn.dataset.postid].likeState = "not-liked"
+    }
+
+    // save changes
+    sessionStorage.setItem("NASA_images", JSON.stringify(images))
+
+    //animation for the heart
 }
 
 async function getImage() {
@@ -36,10 +47,29 @@ async function getImage() {
     return image;
 } 
 
+function loadImages(images) {
+    document.querySelector("#main").innerHTML = ""
+    images.forEach((element, index) => {
+        element["postid"] = index;
+        renderImage(element)
+    });
+}
+
+
+
+
+let images = sessionStorage.getItem("NASA_images")
+if(images && images != "null") {
+    images = JSON.parse(sessionStorage.getItem("NASA_images"))
+} else {
+    images = []
+}
+
 getImage().then(image => {
-    console.log(image);
-    for (prop in image) console.log(prop);
-    appendImage(image);
-    appendImage(image);
-    images.push(image);
+    // add the image only if it is not in the database
+    if (!images.find(el => el.title === image.title)) {
+        image["likeState"] = "not-liked";
+        images.push(image);
+    }
+    loadImages(images)    
 });
